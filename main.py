288b58e832c6c6c17bfa54a2697bc843dee36b36
LIefr1 <cstud129306@vyatsu.ru>
2024-04-17 00:25:15 +0300

    started something

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import cv2
 import torch
-import dlib
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+import matplotlib.pyplot as plt
 
-print("Hello World")
-print(cv2.__version__)
-print(torch.__version__)
-print(dlib.__version__)
+
+cudnn.benchmark = True
+plt.ion()
+
+
+if __name__ == "__main__":
+    model_ft = models.resnet18(weights="IMAGENET1K_V1")
+    num_ftrs = model_ft.fc.in_features
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
+    model_ft.fc = nn.Linear(num_ftrs, 2)
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    model_ft = train_model(
+        model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25
+    )
